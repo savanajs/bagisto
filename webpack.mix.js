95b50318854729a97cf9f54e4f3ba83c68900df3
Rafael Cruz <projetosavanajs@gmail.com>
2020-05-21 00:05:58 -0300

    feat: webpack configs ok

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,4 +1,9 @@
 let mix = require('laravel-mix');
+// const pkg = require('./package.json');
+const ImageminPlugin = require('imagemin-webpack-plugin').default;
+const LiveReloadPlugin = require('webpack-livereload-plugin');
+
+const theme = 'themes/velocity';
 
 /*
  |--------------------------------------------------------------------------
@@ -11,5 +16,26 @@ let mix = require('laravel-mix');
  |
  */
 
-mix.js('resources/assets/js/app.js', 'public/js')
-   .sass('resources/assets/sass/app.scss', 'public/css');
+mix.js('resources/' + theme + '/assets/js/app.js', 'public/'+ theme +'/assets/js')
+   .sass('resources/'+ theme +'/assets/sass/app.scss', 'public/'+ theme +'/assets/css');
+
+mix.browserSync('http://localhost:8000');
+
+// http://localhost:3000
+
+mix.webpackConfig( {
+    plugins: [
+        new LiveReloadPlugin(),
+        new ImageminPlugin( {
+//            disable: process.env.NODE_ENV !== 'production', // Disable during development
+            pngquant: {
+                quality: '95-100',
+            },
+            test: /\.(jpe?g|png|gif|svg)$/i,
+        } ),
+    ],
+});
+
+mix.copy('resources/'+ theme +'/assets/images', 'public/'+ theme +'/assets/images', false );
+
+mix.disableNotifications();
